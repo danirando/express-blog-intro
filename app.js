bcf5282f282036b23b40a8b4bdee111201552a76
danirando <danielerando@tiscali.it>
2025-05-12 13:36:16 +0200

    creato array di post e rotta /bacheca

--- app.js
+++ app.js
@@ -2,6 +2,47 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let posts = [
+  {
+    titolo: "post 1",
+    contenuto: "lorem ipsum",
+    immagine: "/img",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "post 2",
+    contenuto: "lorem ipsum",
+    immagine: "/img",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "post 3",
+    contenuto: "lorem ipsum",
+    immagine: "/img",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "post 4",
+    contenuto: "lorem ipsum",
+    immagine: "/img",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "post 5",
+    contenuto: "lorem ipsum",
+    immagine: "/img",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json(`Server del mio blog`);
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json(posts);
+});
+
 app.listen(port, () => {
   console.log("Il server è in ascolto su http://localhost:" + port);
 });
