81b4a8115419a04d5fe5f5d708adfc03cc3a36bc
danirando <danielerando@tiscali.it>
2025-05-12 13:43:23 +0200

    creata rotta bacheca e cartella con asset statici

--- app.js
+++ app.js
@@ -6,34 +6,36 @@ let posts = [
   {
     titolo: "post 1",
     contenuto: "lorem ipsum",
-    immagine: "/img",
+    immagine: "/img/ciambellone.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "post 2",
     contenuto: "lorem ipsum",
-    immagine: "/img",
+    immagine: "/img/cracker_barbabietola.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "post 3",
     contenuto: "lorem ipsum",
-    immagine: "/img",
+    immagine: "/img/pane_fritto_dolce.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "post 4",
     contenuto: "lorem ipsum",
-    immagine: "/img",
+    immagine: "/img/pasta_barbabietola.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "post 5",
     contenuto: "lorem ipsum",
-    immagine: "/img",
+    immagine: "/img/torta_paesana.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
 ];
+
+app.use(express.static(`public`));
 
 app.get("/", (req, res) => {
   res.json(`Server del mio blog`);
