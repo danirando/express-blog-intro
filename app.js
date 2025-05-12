8e8d454eecb8387948e98460809990327fb08fec
danirando <danielerando@tiscali.it>
2025-05-12 15:24:18 +0200

    fix oggetto posts

--- app.js
+++ app.js
@@ -2,38 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let posts = [
-  {
-    titolo: "post 1",
-    contenuto: "lorem ipsum",
-    immagine: "/img/ciambellone.jpeg",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-  {
-    titolo: "post 2",
-    contenuto: "lorem ipsum",
-    immagine: "/img/cracker_barbabietola.jpeg",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-  {
-    titolo: "post 3",
-    contenuto: "lorem ipsum",
-    immagine: "/img/pane_fritto_dolce.jpeg",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-  {
-    titolo: "post 4",
-    contenuto: "lorem ipsum",
-    immagine: "/img/pasta_barbabietola.jpeg",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-  {
-    titolo: "post 5",
-    contenuto: "lorem ipsum",
-    immagine: "/img/torta_paesana.jpeg",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-];
+let posts = {
+  posts: [
+    {
+      titolo: "post 1",
+      contenuto: "lorem ipsum",
+      immagine: "/img/ciambellone.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 2",
+      contenuto: "lorem ipsum",
+      immagine: "/img/cracker_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 3",
+      contenuto: "lorem ipsum",
+      immagine: "/img/pane_fritto_dolce.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 4",
+      contenuto: "lorem ipsum",
+      immagine: "/img/pasta_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 5",
+      contenuto: "lorem ipsum",
+      immagine: "/img/torta_paesana.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+  ],
+};
 
 app.use(express.static(`public`));
 
@@ -42,7 +44,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bacheca", (req, res) => {
-  res.json({ posts });
+  res.json(posts);
 });
 
 app.listen(port, () => {
