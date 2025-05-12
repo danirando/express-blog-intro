777173f6811e66c36e7450c004e2ab021cb8b350
danirando <danielerando@tiscali.it>
2025-05-12 16:54:55 +0200

    sposto oggetto posts in main.js e lo importo in app.js

---
+++ main.js
@@ -0,0 +1,36 @@
+let posts = {
+  posts: [
+    {
+      titolo: "post 1",
+      contenuto: "lorem ipsum",
+      immagine: "/img/ciambellone.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 2",
+      contenuto: "lorem ipsum",
+      immagine: "/img/cracker_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 3",
+      contenuto: "lorem ipsum",
+      immagine: "/img/pane_fritto_dolce.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 4",
+      contenuto: "lorem ipsum",
+      immagine: "/img/pasta_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "post 5",
+      contenuto: "lorem ipsum",
+      immagine: "/img/torta_paesana.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+  ],
+};
+
+module.exports = { posts };
