4089005298d62b814dc08b41b7e19d89533a3a54
danirando <danielerando@tiscali.it>
2025-05-13 12:21:36 +0200

    creo carella routers

--- app.js
+++ app.js
@@ -2,17 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const { posts } = require("./main.js");
-
 app.use(express.static(`public`));
-
-app.get("/", (req, res) => {
-  res.json(`Server del mio blog`);
-});
-
-app.get("/bacheca", (req, res) => {
-  res.json(posts);
-});
 
 app.listen(port, () => {
   console.log("Il server è in ascolto su http://localhost:" + port);
